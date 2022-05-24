ea4923a052caaf8ff5e97e60e2f6f8afb128f9ae
apedroza <ponchopdev@gmai.com>
2022-05-24 14:08:09 -0500

    Copy/url changes

--- src/sections/Works/React.jsx
+++ src/sections/Works/React.jsx
@@ -11,7 +11,7 @@ export const reactWorks = {
       <Grid container spacing={3}>
         <Grid item md>
           <ProjectCard
-            body="Interior design preview tool, with lots of variants and cool stuff! Has it's own app (unplublished per client request)"
+            body="Interior design preview tool, with lots of variants and cool stuff! Has it's own app (not on any store per client request) and a web version."
             heading="QuarellaDesign"
             overline="Quarella & Decor"
             image="https://yt3.ggpht.com/ytc/AAUvwngkCAxYei50qACVdXnEFzcUKfzjxHDq5sBdAoUH=s900-c-k-c0x00ffffff-no-rj"
@@ -30,19 +30,19 @@ export const reactWorks = {
         </Grid>
         <Grid item md>
           <ProjectCard
-            body="IP Infrignments reporting portal made with React, Redux Toolkit and a PHP API, don't worry if you can't access, it's still on closed beta."
+            body="IP Infrignments reporting portal made with React, Redux Toolkit and a PHP API"
             heading="Reporting Portal"
             overline="Etsy"
             image="https://icon-library.com/images/2018/3892730_etsy-logo-transparent-etsy-circle-logo-png-png.png"
             extra="May, 2021 - Oct 2021"
-            link="https://www.etsy.com/ipmanager/"
+            link="https://www.etsy.com/ipreporting"
           />
         </Grid>
       </Grid>
       <Grid container className="card-row" spacing={3}>
         <Grid item md>
           <ProjectCard
-            body="Small personal project I did on a boring afternoon (like 15 minutes actually, is really not that complex). WARNING: I do NOT own or regulate the contents of the screenshots."
+            body="Small personal project I did on a boring afternoon (like 15 minutes actually, is really not that complex). WARNING: I do NOT own nor regulate the contents of the screenshots."
             heading="Random Screenshot"
             overline="Myself"
             image="http://random-screenshot.herokuapp.com/apple-touch-icon.png"
